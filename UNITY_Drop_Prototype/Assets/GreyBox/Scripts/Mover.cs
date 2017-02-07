f8e73e9517230b2bfd90d14a6a53e4b8aa204825
PinaIacopino <g.iacopino@outlook.com>
2017-02-07 11:22:37 +1100

    Unity Prototype added just in case. Should not be inculded in master branch

---
+++ UNITY_Drop_Prototype/Assets/GreyBox/Scripts/Mover.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public Rail_Movement rail;
+
+    private int currentSeg;
+    private float transition;
+    private bool isCompleted;
+
+    private void Update()
+    {
+        if (!rail)
+            return;
+
+        if (!isCompleted)
+            Play();
+    }
+
+    private void Play()
+    {
+        transition += Time.deltaTime * 1 / 2.5f;
+
+        if (transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+        else if (transition < 0)
+        {
+            transition = 1;
+            currentSeg--;
+        }
+
+        transform.position = rail.CatmullPosition(currentSeg, transition);
+        transform.rotation = rail.Orientation(currentSeg, transition);
+
+    }
+}
---
+++ UNITY_Drop_Prototype/Assets/GreyBox/Scripts/Rail_Movement.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[ExecuteInEditMode]
+
+public class Rail_Movement : MonoBehaviour
+{
+    //Summary
+    //Will move the camera along waypoints.
+    //This script should be attached to a parent object, as its children will become nodes. 
+    // Seg = segments, which is the space between the nodes
+
+    //the waypoints of the path that the camera will travel along
+    private Transform[] nodes;
+
+
+    void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+    }
+
+    // movement between nodes without smoothing
+    public Vector3 LinearPosition(int seg, float ratio)
+    {
+        Vector3 p1 = nodes[seg].position;
+        Vector3 p2 = nodes[seg + 1].position;
+
+        return Vector3.Lerp(p1, p2, ratio);
+    }
+
+    //smooths movement between segments
+    //needs at least 4 points to work
+    public Vector3 CatmullPosition(int seg, float ratio)
+    {
+        Vector3 p1, p2, p3, p4;
+
+        if (seg == 0)
+        {
+            p1 = nodes[seg].position;
+            p2 = p1;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 2].position;
+        }
+        else if (seg == nodes.Length - 2)
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = p3;
+        }
+        else
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 2].position;
+        }
+
+        float t2 = ratio * ratio;
+        float t3 = t2 * ratio;
+
+        float x = 0.5f * ((2.0f * p2.x) + (-p1.x + p3.x) * ratio + (2.0f * p1.x - 5.0f * p2.x + 4 * p3.x - p4.x) * t2 + (-p1.x + 3.0f * p2.x - 3.0f * p3.x + p4.x) * t3);
+        float y = 0.5f * ((2.0f * p2.y) + (-p1.y + p3.y) * ratio + (2.0f * p1.y - 5.0f * p2.y + 4 * p3.y - p4.y) * t2 + (-p1.y + 3.0f * p2.y - 3.0f * p3.y + p4.y) * t3);
+        float z = 0.5f * ((2.0f * p2.z) + (-p1.z + p3.z) * ratio + (2.0f * p1.z - 5.0f * p2.z + 4 * p3.z - p4.z) * t2 + (-p1.z + 3.0f * p2.z - 3.0f * p3.z + p4.z) * t3);
+
+        return new Vector3(x, y, z);
+    }
+
+
+    //Controls rotation of object that is folling the path
+    public Quaternion Orientation(int seg, float ratio)
+    {
+        Quaternion q1 = nodes[seg].rotation;
+        Quaternion q2 = nodes[seg + 1].rotation;
+
+        return Quaternion.Lerp(q1, q2, ratio);
+    }
+
+    // Draws lines in the editor to show the path the player will take
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < nodes.Length - 1; i++)
+        {
+            Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 3.0f);
+        }
+    }
+}
